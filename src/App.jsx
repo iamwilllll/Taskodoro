6157dcbde824320572f4dfd4daa778802cf3f328
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-08-23 18:11:07 -0400

    Add layout sections to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import HeaderSection from './layout/HeaderSection';
+import TaskSection from './layout/TaskSection';
+import TimerSection from './layout/TimerSection';
+
 function App() {
-    return <></>;
+    return (
+        <div className="flex flex-col h-screen p-5">
+            <HeaderSection />
+
+            <div className="flex-1 flex flex-col md:flex-row">
+                <TimerSection />
+                <TaskSection />
+            </div>
+        </div>
+    );
 }
 
 export default App;
