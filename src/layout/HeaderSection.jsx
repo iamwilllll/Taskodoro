6b3aafa05d41f4adb0704ddb803e8e453c244ed6
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-08-24 15:45:12 -0400

    style: Change classes to make the page responsive and add styles to the header buttons.

--- src/layout/HeaderSection.jsx
+++ src/layout/HeaderSection.jsx
@@ -3,19 +3,19 @@ import OpenTaskIcon from '../components/icons/OpenTaskIcon';
 
 function HeaderSection() {
     return (
-        <section className="md:p-5 flex flex-col-reverse gap-10 md:flex-row md:justify-between">
-            <aside className="text-xl text-primary-font-color md:text-3xl md:flex md:items-center">
-                <h1 className="text-center md:text-left flex flex-col md:flex-row md:gap-5">
-                    <span className="block md:inline font-extrabold">TASKODORO</span>
-                    <span className="hidden md:inline font-normal">/</span>
-                    <span className="block md:inline font-light">YOUR GOALS FOR THIS SESSION</span>
+        <section className="lg:p-5 flex flex-col-reverse gap-10 lg:flex-row lg:justify-between">
+            <aside className="text-xl text-primary-font-color lg:text-3xl lg:flex lg:items-center">
+                <h1 className="text-center lg:text-left flex flex-col lg:flex-row lg:gap-5">
+                    <span className="block lg:inline font-extrabold text-4xl">TASKODORO</span>
+                    <span className="hidden lg:inline font-normal">/</span>
+                    <span className="block lg:inline font-light text-3xl">YOUR GOALS FOR THIS SESSION</span>
                 </h1>
             </aside>
             <aside className="flex justify-between">
-                <button className="md:hidden">
-                    <OpenTaskIcon width={50} height={50} />
+                <button className="lg:hidden">
+                    <OpenTaskIcon width={50} height={50} className="text-primary-color" />
                 </button>
-                <button className="hover:cursor-pointer hover:scale-125 transition-transform">
+                <button className=" hover:scale-110 hover:cursor-pointer hover:brightness-110 transition-transform transition-color text-primary-color">
                     <SettingIcon width={50} height={50} />
                 </button>
             </aside>
