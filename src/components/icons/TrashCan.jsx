6dff8f3d450931361f0504c2a93c360ed60fef6d
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-08-24 16:14:00 -0400

    fix: Update printWidth in .prettierrc and adjust HTML theme; refactor icon components for consistency

--- src/components/icons/TrashCan.jsx
+++ src/components/icons/TrashCan.jsx
@@ -1,6 +1,10 @@
 const TrashCan = props => (
     <svg xmlns="http://www.w3.org/2000/svg" width={128} height={128} viewBox="0 0 512 512" {...props}>
-        <path fill="currentColor" fillRule="evenodd" d="M320 85.334H192V42.667h128zm-85.333 128H192V384h42.667zm85.333 0h-42.667V384H320zM448 128v42.667h-42.667v298.667H106.667V170.667H64V128zm-85.333 42.667H149.333v256h213.334z" />
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M320 85.334H192V42.667h128zm-85.333 128H192V384h42.667zm85.333 0h-42.667V384H320zM448 128v42.667h-42.667v298.667H106.667V170.667H64V128zm-85.333 42.667H149.333v256h213.334z"
+        />
     </svg>
 );
 export default TrashCan;
--- src/layout/TimerSection.jsx
+++ src/layout/TimerSection.jsx
@@ -1,15 +1,24 @@
 import PauseIcon from '../components/icons/PauseIcon';
 import SoundOn from '../components/icons/SoundOn';
+
 function TimerSection() {
     return (
-        <section className="w-min-1/2 w-full flex flex-col items-center justify-center flex-1 gap-10">
-            <header className="text-7xl text-primary-font-color">25:00</header>
+        <section className="flex flex-col items-center justify-center lg:max-w-1/2 w-1/2 gap-10">
+            <header className="text-7xl font-bold text-primary-font-color">25:00</header>
             <footer className="flex gap-5 bg-secondary-bg py-5 px-15 rounded-full">
                 <button>
-                    <PauseIcon width={70} height={70} />
+                    <PauseIcon
+                        width={70}
+                        height={70}
+                        className="text-primary-color hover:scale-110 hover:cursor-pointer hover:brightness-110 transition-transform transition-color"
+                    />
                 </button>
                 <button>
-                    <SoundOn width={70} height={70} />
+                    <SoundOn
+                        width={70}
+                        height={70}
+                        className="text-primary-color hover:scale-110 hover:cursor-pointer hover:brightness-110 transition-transform transition-color"
+                    />
                 </button>
             </footer>
         </section>
